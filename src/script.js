709d28dfcfb432b9447a00fea225b0ba07aaa530
Yulia Zas <zasyulia@gmail.com>
2022-08-21 21:48:31 +0300

    updated JS file

--- src/script.js
+++ src/script.js
@@ -79,3 +79,5 @@ fahrenheitLink.addEventListener("click", displayFahrenheit);
 
 let celsiusLink = document.querySelector("#celsius");
 celsiusLink.addEventListener("click", displayCelsius);
+
+search("Kyiv");
