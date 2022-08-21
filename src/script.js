9293bc698059a5bc165c7c8531f02ab1cebfccb7
Yulia Zas <zasyulia@gmail.com>
2022-08-21 20:00:50 +0300

    Added search engine

--- src/script.js
+++ src/script.js
@@ -43,8 +43,18 @@ function displayTemperature(response) {
   );
 }
 
-let apiKey = "b5b3a26b2b84af69bb9f76f3d9b2d652";
-let apiUrl =
-  "https://api.openweathermap.org/data/2.5/weather?q=New York,usa&appid=b5b3a26b2b84af69bb9f76f3d9b2d652&units=metric";
+function search(city) {
+  let apiKey = "b5b3a26b2b84af69bb9f76f3d9b2d652";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-axios.get(apiUrl).then(displayTemperature);
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
