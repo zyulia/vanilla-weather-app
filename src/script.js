fa9a32a714d920b79b5b5946057336a3c8e1890b
Yulia Zas <zasyulia@gmail.com>
2022-08-28 17:09:00 +0300

    Updated css file

--- src/script.js
+++ src/script.js
@@ -81,7 +81,7 @@ function displayForecast(response) {
                     forecastDay.weather[0].icon
                   }@2x.png"
                   alt=""
-                  width="42"
+                  width="48"
                 />
                 <div class="weather-forecast-temperature">
                   <span class="weather-forecast-max">${Math.round(
